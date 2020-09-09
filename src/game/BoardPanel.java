20e91a66b827c8083dd815bbf6c88a4e43805fb2
Theo <theoriandy.17@gmail.com>
2020-09-10 09:20:41 +1000

    updates

--- src/game/BoardPanel.java
+++ src/game/BoardPanel.java
@@ -3,7 +3,6 @@ package game;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.HashMap;
--- src/packets/PlayersUpdatePacket.java
+++ src/packets/PlayersUpdatePacket.java
@@ -3,7 +3,6 @@ package packets;
 import java.io.Serializable;
 import java.util.HashMap;
 
-import game.Grid;
 import game.Player;
 import game.Position;
 
@@ -16,30 +15,10 @@ public class PlayersUpdatePacket implements Serializable{
 	
 	public HashMap<Integer,Boolean> readyStatus;
 	public HashMap<Integer,Position> clientsPosition;
-	
-	public HashMap<Integer, Player> clientsPlayerInfo = new HashMap<Integer, Player>();
 
 	public PlayersUpdatePacket(HashMap<Integer, Boolean> readyStatus, HashMap<Integer, Position> clientsPosition) {
 		this.readyStatus = readyStatus;
 		this.clientsPosition = clientsPosition;
-		
-	}
-	
-	public PlayersUpdatePacket(HashMap<Integer, Boolean> readyStatus, HashMap<Integer, Position> clientsPosition, Grid grid) {
-		this.readyStatus = readyStatus;
-		this.clientsPosition = clientsPosition;
-		
-		for(int i : this.clientsPosition.keySet()) {
-			try {
-				Player p = new Player(grid, this.clientsPosition.get(i).getRow(), this.clientsPosition.get(i).getCol());
-				
-				clientsPlayerInfo.put(i, p);
-				
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			
-		}
 		
 	}
 }
