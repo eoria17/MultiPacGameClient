681c8182a7eec877494d8b0ac39d4f505affbc3c
Sam-Xxx <70259047+Sam-Xxx@users.noreply.github.com>
2020-09-21 21:20:20 +0800

    This is signle play mode file

---
+++ SignleGame/Monster.java
@@ -0,0 +1,22 @@
+package SignleGame;
+
+public class Monster extends Moveable {
+    private boolean canView = true;  // allows
+    private Player player;
+    public Monster(Grid g, Player p, int row, int col) throws Exception
+    {
+        super(g);
+        player = p;
+        setCell(grid.getCell(row,col));
+    }
+    public Position move()
+    {
+        currentDirection = grid.getBestDirection(currentCell, player.getCell());
+        currentCell = (grid.getCell(getCell(),getDirection()));
+        return currentCell;
+    }
+    public boolean viewable()  // can be used for hiding
+    {
+        return canView;
+    }
+}
